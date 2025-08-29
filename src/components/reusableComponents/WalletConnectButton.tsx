e489c5c43445d915e398a847aee69012a4c4399f
Anurag Ojha <anuragojha8435@gmail.com>
2025-08-29 23:59:02 +0530

    removed name for connected wallet

--- src/components/reusableComponents/WalletConnectButton.tsx
+++ src/components/reusableComponents/WalletConnectButton.tsx
@@ -66,7 +66,7 @@ export function WalletConnectButton() {
                   )}
                 </div>
               )}
-              {chain.name}
+              {/* {chain.name} */}
             </button>
 
             <button
